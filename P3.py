8d1ffbad84d22a23e8a97bc3458b1ed109becc76
GullySock <47152780+GullySock@users.noreply.github.com>
2019-02-20 21:29:11 -0600

    Update P3.py

--- P3.py
+++ P3.py
@@ -1,11 +1,12 @@
 from urllib.request import urlretrieve
+import re 
 
 urlretrieve ('https://s3.amazonaws.com/tcmg476/http_access_log', 'awslog')
 
-def main ():
-    log = open("testlog.txt" , "r")
-    for line in log:
-        print (line)
-    print ('caleb')
-        
 
+log = open("testlog.txt" , "r")
+
+
+
+for line in log:
+    print (line)
