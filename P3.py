29029c4561abe513c4f0bf11bfdf8fb65d0379c8
GullySock <47152780+GullySock@users.noreply.github.com>
2019-02-21 00:37:08 -0600

    Update P3.py
    I figured out how to make the regex take variables, so I can easily look through months and things.

--- P3.py
+++ P3.py
@@ -1,15 +1,22 @@
-from urllib.request import urlretrieve
+##from urllib.request import urlretrieve
 import re 
 i = 0 
 
-urlretrieve ('https://s3.amazonaws.com/tcmg476/http_access_log', 'awslog')
+##urlretrieve ('https://s3.amazonaws.com/tcmg476/http_access_log', 'awslog')
 
 
 
 log = open("testlog.txt" , "r")
 
-
+## this goes through the lines, prints and counts them. 
 for line in log:
     print (line)
+    a = 24
+    x = re.findall("(local|remote) (- -) (.{}.Oct.1994.)".format(a), line)
+    print (x)
+    
+    ##this answers number 1
     i = i+1
     print (i)
+    
+    
