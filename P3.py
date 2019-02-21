1c25e6c1963368e68d53e6a733ec5581e7fb4ff8
GullySock <47152780+GullySock@users.noreply.github.com>
2019-02-20 23:25:21 -0600

    Update P3.py
    I added the ability to count the line number, so you # of requests

--- P3.py
+++ P3.py
@@ -1,5 +1,6 @@
 from urllib.request import urlretrieve
 import re 
+i = 0 
 
 urlretrieve ('https://s3.amazonaws.com/tcmg476/http_access_log', 'awslog')
 
@@ -10,3 +11,5 @@ log = open("testlog.txt" , "r")
 
 for line in log:
     print (line)
+    i = i+1
+    print (i)
