4fbfdd8b707d20346f88020219d7b3c64daad022
GullySock <47152780+GullySock@users.noreply.github.com>
2019-02-25 17:11:01 -0600

    Update P3.py
    A lot of this code is superflous, but at line 17, you'll see my regex code, and this is what I'll be using as a template from here on out

--- P3.py
+++ P3.py
@@ -1,6 +1,8 @@
 ##from urllib.request import urlretrieve
 import re 
-i = 0 
+linenum = 0 
+day = 1
+months = ["error", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 ##urlretrieve ('https://s3.amazonaws.com/tcmg476/http_access_log', 'awslog')
 
@@ -10,13 +12,15 @@ log = open("testlog.txt" , "r")
 
 ## this goes through the lines, prints and counts them. 
 for line in log:
-    print (line)
-    a = 24
-    x = re.findall("(local|remote) (- -) (.{}.Oct.1994.)".format(a), line)
-    print (x)
+    ##print (line)
+    
+    if re.search(("([a-z]+)( - )[a-z.-]+( \[)(..\/Oct\/[0-9:]+)( [0-9-]+\])( .[a-zA-Z]+ )([a-z0-9.]+)( [a-zA-Z0-9./]+. )([0-9]..)( [0-9]+)"), line):
+        print (line)    
+                
+       
     
     ##this answers number 1
-    i = i+1
-    print (i)
+    linenum = linenum+1
+    print (linenum)
     
     
